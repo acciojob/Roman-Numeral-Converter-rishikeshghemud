6fe9ca8109317fd94ab8ec3b5b2672a680aa830e
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-16 00:38:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,23 +11,39 @@ function convertToRoman(num) {
 
   //your code here
 
-	let str = '';
+	 if (num === 0) return ""; // Romans had no zero
 
-	for(const [key, value] of Object.entries(obj)) {
-		while(num >= value) {
-			str += key;
-			num -= value;
-		}
-	}
+    // Build full roman mapping including subtractives
+    const romans = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
-	return str;
+    let result = "";
 
+    for (let [symbol, value] of romans) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
-
-
 
 
 // do not edit below this line
